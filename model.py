316e7b5c97d9ab4210b1825ba96990fd5af380df
Stormtrooperroman <stormtrooperroman@gmail.com>
2023-01-14 12:50:37 +0700

    First commit

---
+++ model.py
@@ -0,0 +1,50 @@
+import numpy as np
+from update_rules import hebbian, anti_hebbian, random_walk
+from math import sqrt
+
+class TPM:
+    def __init__(self, input_size=4, hidden_size=3, l=6):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.l = l
+        self.weights = np.random.randint(-l, l+1, [hidden_size, input_size])
+        self.sigma = 0
+        self.tau = 0
+        self.input_val = None
+
+    def get_output(self, input_val):
+
+        hidden_size = self.hidden_size
+        input_size = self.input_size
+        weights = self.weights
+
+        input_val = input_val.reshape([hidden_size, input_size])
+
+        sigma = np.sign(np.sum(input_val * weights, axis=1) * (1/sqrt(len(input_val))))
+        for i, _ in np.ndenumerate(sigma):
+            if sigma[i] == 0:
+                sigma[i] = -1
+
+        tau = np.prod(sigma)
+        self.input_val = input_val
+        self.sigma = sigma
+        self.tau = tau
+
+        return self.tau
+
+    def __call__(self, input_val):
+        return self.get_output(input_val)
+
+    def update(self, second_tau, update_rule='hebbian'):
+
+        if self.tau == second_tau:
+            if update_rule == 'hebbian':
+                self.weights = hebbian(self.weights, self.input_val, self.sigma, self.tau, second_tau, self.l)
+
+            elif update_rule == 'anti_hebbian':
+                self.weights = anti_hebbian(self.weights, self.input_val, self.sigma, self.tau, second_tau, self.l)
+
+            elif update_rule == 'random_walk':
+                self.weights = random_walk(self.weights, self.input_val, self.sigma, self.tau, second_tau, self.l)
+            else:
+                raise Exception("Invalid update rule.")
---
+++ update_rules.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def theta(t1, t2):
+    return 1 if t1 == t2 else 0
+
+
+def hebbian(weights, input_val, sigma, tau1, tau2, l):
+    for (i, j), _ in np.ndenumerate(weights):
+        weights[i, j] = weights[i, j] + input_val[i, j] * tau1 * theta(sigma[i], tau1) * theta(tau1, tau2)
+        weights[i, j] = np.clip(weights[i, j], -l, l)
+    return weights
+
+
+def anti_hebbian(weights, input_val, sigma, tau1, tau2, l):
+
+    for (i, j), _ in np.ndenumerate(weights):
+        weights[i, j] -= input_val[i, j] * tau1 * theta(sigma[i], tau1) * theta(tau1, tau2)
+        weights[i, j] = np.clip(weights[i, j], -l, l)
+    return weights
+
+
+def random_walk(weights, input_val, sigma, tau1, tau2, l):
+    for (i, j), _ in np.ndenumerate(weights):
+        weights[i, j] += input_val[i, j] * theta(sigma[i], tau1) * theta(tau1, tau2)
+        weights[i, j] = np.clip(weights[i, j], -l, l)
+    return weights
+
